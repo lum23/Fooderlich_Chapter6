496783b32f75c2a45086da2c7bbb01da57e37a4e
Gap08 <cassylum23@gmail.com>
2023-03-22 11:03:57 +0800

    chapter 6

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'fooderlich_theme.dart';
+import 'home.dart';
+import 'package:provider/provider.dart';
+import 'models/models.dart';
+
+//
+// void main() {
+//   runApp(const MyApp());
+// }
+
+void main() {
+  runApp(const Fooderlich());
+}
+
+class Fooderlich extends StatelessWidget {
+  const Fooderlich({super.key});
+  @override
+  Widget build(BuildContext context) {
+    final theme = FooderlichTheme.dark();
+    return MaterialApp(
+      theme: theme,
+      title: 'Fooderlich',
+      // 1
+      home: MultiProvider(
+        providers: [
+          // 2
+          ChangeNotifierProvider(create: (context) => TabManager()),
+          ChangeNotifierProvider(create: (context) => GroceryManager()),
+        ],
+        child: const Home(),
+      ),
+      // home: const Home(),
+    );
+  }
+}
